f8692a9de7b3527b36384201723fad2eb9750893
Kin <winterheat@gmail.com>
2023-03-20 03:08:50 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const mockedData = [
+  { id: 1, title: "ha", url: "nano2.jpg" },
+  { id: 2, title: "haha", url: "nano1.png" },
+];
+
+function fetch2(url) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(mockedData), 1000);
+  });
+}
+
 function App() {
-  return <div className="App">This is a hello world</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch2("someURL").then((v) => setData(v));
+  }, []);
+
+  return (
+    <div className="App">
+      {data == null && "Loading..."}
+      {data?.map((e, i) => (
+        <li key={e.id}>
+          {e.title} {e.url}
+        </li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
