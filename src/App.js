bfedf5a0861f55c36023e27060c4fc2ecc270293
Kin <winterheat@gmail.com>
2023-03-20 03:19:15 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-const mockedData = [
-  { id: 1, title: "ha", url: "nano2.jpg" },
-  { id: 2, title: "haha", url: "nano1.png" },
-];
+const mockedData = Array.from({ length: 12 }, (e, i) => {
+  return {
+    id: i,
+    title: `title ${i + 1}`,
+    url: i % 2 === 0 ? "nano1.png" : "nano2.jpg",
+  };
+});
+
+// [
+//   { id: 1, title: "ha", url: "nano2.jpg" },
+//   { id: 2, title: "haha", url: "nano1.png" },
+// ];
 
 function fetch2(url) {
   return new Promise((resolve) => {
@@ -25,7 +33,7 @@ function App() {
       {data?.map((e, i) => (
         <div className="grid-box" key={e.id}>
           <img src={e.url} />
-          {e.title} 
+          {e.title}
         </div>
       ))}
     </div>
